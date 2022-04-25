de8fb7c3e06f751185c997eadfb1fab76a49eb65
Nishant Indal <nishantindal@gmail.com>
2022-04-26 00:42:35 +0530

    latency improved

--- app/util/translate.js
+++ app/util/translate.js
@@ -1,3 +1,5 @@
+const res = require('express/lib/response');
+
 const {Translate} = require('@google-cloud/translate').v2;
 require('dotenv').config();
 
@@ -13,13 +15,23 @@ const translate = new Translate({
 async function translateText(data, targetLanguage){
     try {
         const promise = [];
+        const result = [];
         for (element of data ){
             for (const key in element) {
-                if( key  == 'mobileNumber') continue;
-                [element[key]] = await translate.translate(element[key], targetLanguage);
+                 promise.push(translate.translate(element[key], targetLanguage));
             }
         };
-        return data;
+        const arr = await Promise.all(promise);
+        for ( let i=0 ;i<promise.length;i=i+5){
+            const obj = {};
+            obj.name = arr[i][0];
+            obj.mobileNulber = arr[i+1][0];
+            obj.state_name = arr[i+2][0];
+            obj.district_name = arr[i+3][0];
+            obj.village_name = arr[i+4][0];
+            result.push(obj);
+        }
+        return result;
     } catch (error) {
         console.log(`Error at translateText --> ${error}`);
         return 0;
